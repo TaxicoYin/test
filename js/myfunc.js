55bbcdccea0e203ffe71339ca798d208315e7777
yin <dr.bart@live.com>
2021-03-04 11:35:28 +0800

    upadate encryption from sha1 to sha256

--- js/myfunc.js
+++ js/myfunc.js
@@ -2,22 +2,19 @@ function check()
 
 {
 var pwd=document.getElementById('pass').value;
-var sha=hex_sha1(pwd)
+var sha=sha512(pwd)
 if (pwd==""){
 document.getElementById('p1').innerHTML="Ooops,looks like you didn't type anything"
 }
+if (pwd=="ftcsb"){
+	document.getElementById('p1').innerHTML="确实，很难不赞同。。。"
+}
 else{
-	if (sha=="fc7a734dba518f032608dfeb04f4eeb79f025aa7")
-	login()
+	if (sha=="a7186c57d8d0444f78792730bb14ba1e5fe6e0efa0d84fb829bf4273b7283958fe293dda04d05e6eb88562bab7f7ad83da13e3e04639bc200b1b3a634b7d3979")
+		window.location.href="note.html";
 	else
 	document.getElementById('p1').innerHTML="Wrong password!"	
 	}
-}
-function login()
-{
-	
-　　window.location.href="note.html";
-
 }
 function dokeydown() 
     { 
